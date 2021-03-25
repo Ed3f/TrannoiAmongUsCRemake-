212d1a3058272d6bb26167e1bf810b7a43ed4f17
FedeRoss01 <74314405+FedeRoss01@users.noreply.github.com>
2021-03-25 12:52:26 +0100

    Add files via upload

--- o.c
+++ o.c
@@ -1,40 +1,72 @@
-#include<stdio.h>
-#include<stdlib.h>
-  struct listNode{
-  char data;
-  struct listnode *nextptr;
-};
-void insert(ListNodePtr *sptr ,char item);
-typedef struct listNode ListNode;
-typedef  listNode *ListNodePtr;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define max 10
+typedef enum stato_giocatore{ astronauta, impostore, defenestrato, assassinato};
+typedef enum colore_giocatore{ rosso,giallo, blu, verde, viola, grigio, nero, marrone, rosa, bianco };
+typedef struct {
+	stato_giocatore stato;
+  colore_giocatore colore;
+}player;
+
 int main(){
-  char item;
-ListNodePtr startPtr = NULL;
-printf("inserisci caratteri");
-scanf("%c",&item);
-insert(&startPtr,item);
-}
-void insert(ListNodePtr *sptr ,char value){
-  ListNodePtr new_ptr = malloc(sizeof(ListNode));
-  if (new_ptr != NULL){
-     new_ptr -> data = value;
-     new_ptr -> nextptr= NULL;
-    ListNodePtr preptr = NULL;
-    ListNodePtr courrentptr = *sptr;
-    while ( courrentptr != NULL && value < courrentptr -> data ){
-      preptr = courrentptr;
-      courrentptr = courrentptr -> nextptr;
-    }
-    if (preptr == NULL){
-      new_ptr -> new_ptr = sptr;
-      *sptr = new_ptr;
-    }
-    else{
-      preptr -> nextptr = new_ptr;
-      new_ptr -> nextptr = courrentptr;
-    }
-    else {
-      printf("%c not iserited\n", value);
-    }
-  }
+  player giocatore[max];
+	int n_personaggi;
+	scanf ("%d", &n_personaggi);
+	time_t t;
+	srand((unsigned) time(&t));
+	int personaggi[n_personaggi];
+	for (int i = 0; i < n_personaggi; i++){
+		 personaggi[i] = 0;
+	}
+
+	for (int i = 0; i < n_personaggi; i++){
+		 personaggi[i] = i;
+	}
+
+	 for (int i = 0; i < n_personaggi; i++){
+		 int temp = personaggi[i];
+		 int random = rand() % n_personaggi;
+		 personaggi[i] = personaggi[random];
+		 personaggi[random] = temp;
+	 }
+	 for (int i = 0; i < n_personaggi; i++){
+		 giocatore -> colore = personaggi[i];
+		 printf("%d ",personaggi[i]);
+	}
+	for (int i = 0; i < n_personaggi; i++){
+	switch(personaggi[i]){
+		case rosso:
+		printf("= rosso\n");
+		break;
+		case verde:
+		printf("= verde\n");
+		break;
+		case blu:
+		printf("= blu\n");
+		break;
+		case nero:
+		printf("= nero\n");
+		break;
+		case bianco:
+		printf("= bianco\n");
+		break;
+		case viola:
+		printf("= viola\n");
+		break;
+		case marrone:
+		printf("= marrone\n");
+		break;
+		case grigio:
+		printf("= sirena\n");
+		break;
+		case rosa:
+		printf("= rosa\n");
+		break;
+		case giallo:
+		printf("= giallo\n");
+		break;
+		}
+	}
 }
