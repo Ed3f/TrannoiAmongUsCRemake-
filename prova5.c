4c8aac70864e5bb53220a60ca8a8e9bf27b90930
FedeRoss01 <74314405+FedeRoss01@users.noreply.github.com>
2021-03-31 11:31:11 +0200

    Add files via upload

--- prova5.c
+++ prova5.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include"gamelib.h"
-int *scelta_giocatori(int n_personaggi);
-//void colori(int personaggio);
+typedef struct{
+  struct stanza* ptr_Stanza;
+  enum stato_giocatore  { astronauta, impostore, defenestrato, assassinato}stato;
+  enum nome_giocatore  {rosso, verde, blu, nero, bianco, viola, marrone, arancione, rosa, giallo}colore;
+}player;
+static int scelta_giocatori(int n_personaggi);
+static void colori(int personaggio);
+static void crea_giocatori(int n_personaggi);
+static void scelta_ruolo_giocatore(int n_personaggi);
+static int probabilita(int n_personaggi);
+ player *giocatori;
 int main(){
    int n_personaggi;
-   int *p;
   do {
     printf("BENVENUTI NEL MENÙ D'IMPOSTAZIONE DEL GIOCO!\n");
     printf("Inserire il numero di giocatori\n con un minimo di 4 giocatori ed un massimo di 10\n ");
@@ -15,35 +22,25 @@ int main(){
     switch (n_personaggi) {
 
       case 4 :
-             printf(" 4 giocatori\n");
-              p = scelta_giocatori(n_personaggi);
-               printf("\n");
-             for (int i = 0; i < n_personaggi; i++){
-               printf("%d",*p);
-             }
+             scelta_giocatori(n_personaggi);
+             scelta_ruolo_giocatore( n_personaggi);
              break;
       case 5 :
-             printf(" 5 giocatori\n");
              scelta_giocatori(n_personaggi);
              break;
       case 6 :
-             printf(" 6 giocatori\n");
              scelta_giocatori(n_personaggi);
              break;
       case 7 :
-             printf(" 7 giocatori\n");
              scelta_giocatori(n_personaggi);
              break;
       case 8 :
-             printf(" 8 giocatori\n");
              scelta_giocatori(n_personaggi);
              break;
       case 9 :
-             printf(" 9 giocatori\n");
              scelta_giocatori(n_personaggi);
              break;
       case 10 :
-              printf(" 10 giocatori\n");
               scelta_giocatori(n_personaggi);
               break;
         default :
@@ -56,11 +53,14 @@ int main(){
   }
   while( n_personaggi < 4);
   }
-int *scelta_giocatori(int n_personaggi){
+  static void crea_giocatori(int n_personaggi){
+    giocatori = (player*)malloc(n_personaggi*sizeof(player));
+  }
+static int scelta_giocatori(int n_personaggi){
   time_t t;
   srand((unsigned) time(&t));
+  crea_giocatori(n_personaggi);
   int personaggi[n_personaggi];
-  int* ptr_personaggi;
   for (int i = 0; i < n_personaggi; i++){
      personaggi[i] = 0;
   }
@@ -76,11 +76,13 @@ int *scelta_giocatori(int n_personaggi){
      personaggi[random] = temp;
    }
    for (int i = 0; i < n_personaggi; i++){
-     printf("%d ",personaggi[i]);
-  }
-  return personaggi;
+     giocatori[i].colore = personaggi[i];
+     printf("%d",giocatori[i].colore);
+     colori(giocatori[i].colore);
 }
-/*void colori (int personaggio ){
+
+}
+static void colori (int personaggio ){
   switch(personaggio){
     case rosso:
     printf("= rosso\n");
@@ -113,7 +115,103 @@ int *scelta_giocatori(int n_personaggi){
     printf("= giallo\n");
     break;
   }
-
-
 }
-*/
+static void scelta_ruolo_giocatore(int n_personaggi){
+int personaggi[n_personaggi];
+  int n_impostori;
+    n_impostori = probabilita(n_personaggi);
+  int impostori[n_impostori];
+  for (int i = 0; i < n_personaggi; i++){
+  personaggi[i] = giocatori[i].colore;
+  }
+  for (int i = 0; i < n_personaggi; i++){
+  printf("%d\n", personaggi[i]);
+  }
+    for (int i = 0; i < n_impostori; i++){
+      int temp = personaggi[i];
+      int posizione = rand() % n_personaggi;
+          impostori[i] = personaggi[posizione];
+          personaggi[posizione] = temp;
+        }
+        printf("1\n");
+        for (int i = 0; i < n_personaggi; i++){
+          if (impostori[i] == personaggi[i]){
+          giocatori[i].stato = 1;
+        printf("personaggio %d è l'impostore%d\n",impostori[i],giocatori[i].stato);
+        }
+        printf("2\n");
+        }
+        int n_astronauti = n_personaggi - n_impostori;
+        int astronauta[n_astronauti];
+        int rimanenti;
+        printf("3\n");
+          switch (n_impostori) {
+            case 1:
+            for(int i = 0; i < n_astronauti; i++){
+              if (personaggi[i] != impostori[0]){
+                astronauta [i] = personaggi[i];
+                giocatori[i].stato = 0;
+                printf("personaggio %d è l'Astronauta %d\n", astronauta[i],giocatori[i].stato);
+          }
+        }
+          printf("4\n");
+          break;
+          case 2:
+              for(int i = 0; i < n_astronauti; i++){
+            if (personaggi[i] != impostori[0] && personaggi[i] != impostori[1]){
+              astronauta [i] = personaggi[i];
+              giocatori[i].stato = 0;
+            printf("personaggio %d  è l'Astronauta %d\n",astronauta[i],giocatori[i].stato);
+        }
+      }
+        printf("5\n");
+        break;
+       case 3:
+           for(int i = 0; i < n_astronauti; i++){
+         if (personaggi[i] != impostori[0] && personaggi[i] !=impostori[1] && personaggi[i] !=impostori[2]){
+           astronauta [i] = personaggi[i];
+           giocatori[i].stato = 0;
+           printf("personaggio %d  è l'Astronauta %d \n", astronauta[i],giocatori[i].stato);
+     }
+   }
+     printf("6\n");
+     break;
+    }
+        }
+int  probabilita (int n_personaggi){
+  int n_impostori;
+if (n_personaggi >= 4 && n_personaggi <= 6){
+   if ((rand()%100) <= 5){
+     n_impostori = 3;
+   }
+   else if ((rand()%100) >= 6 && (rand()%100) <= 45){
+     n_impostori = 2;
+   }
+   else if ((rand()%100) >= 46 && (rand()%100) <= 100){
+     n_impostori = 1;
+   }
+}
+  if (n_personaggi > 6 && n_personaggi <= 7){
+          if ((rand()%100) < 15){
+           n_impostori = 3;
+        }
+        else if ( (rand()%100) >= 15 && (rand()%100) <= 65){
+         n_impostori = 2;
+        }
+        else if ( (rand()%100) > 65 && (rand()%100) <=100){
+           n_impostori = 1;
+    }
+  }
+  if ( 7 < n_personaggi <= 10){
+          if ((rand()%100) < 75){
+           n_impostori = 3;
+        }
+        else if ( (rand()%100) >= 45 && (rand()%100) <= 100){
+         n_impostori = 2;
+        }
+        else if ( (rand()%100)  >= 15 && (rand()%100) <= 25){
+           n_impostori = 1;
+    }
+  }
+    return(n_impostori);
+  }
